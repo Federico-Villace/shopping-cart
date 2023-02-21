b4b3075dfab439854a8ef93fca997a1f892d491a
Federico Villace <federicovillace@gmail.com>
2023-02-21 13:02:01 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { Products } from "./components/products";
-import { products } from "./mocks/products.json";
+import { products as initialProducts } from "./mocks/products.json";
 
 function App() {
+  const [products] = useState(initialProducts);
+  const [filters, setFilter] = useState({
+    category: "all",
+    minPrice: 0,
+  });
+
+  const filteredProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+  };
+
   return (
     <main>
-      <Products products={products} />
+      <Products products={filteredProducts(products)} />
     </main>
   );
 }
