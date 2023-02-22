a31f5b28365fccd59598d3b29e6ef79613020810
Federico Villace <federicovillace@gmail.com>
2023-02-22 20:34:29 -0300

    create cart context

---
+++ src/context/cart.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    //setCart([...cart, product])
+    const productInCartIndex = cart.findIndex((item) => item.id === product.id);
+    if (productInCartIndex >= 0) {
+      const newCart = structuredClone(cart);
+      newCart[productInCartIndex].quatity += 1;
+      return setCart(newCart);
+    }
+    setCart((prevState) => [...prevState, { ...product, quantity: 1 }]);
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
