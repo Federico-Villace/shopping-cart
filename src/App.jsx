f8a11a3093fe87c55722f35c0b0fd61332f7430b
Federico Villace <federicovillace@gmail.com>
2023-03-06 17:45:32 -0300

    update source of products to test

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import { Products } from "./components/Products/ProductsComponent";
 import { useFilters } from "./hooks/useFilters";
 import { Cart } from "./components/Cart/CartComponent";
 import { CartProvider } from "./context/cart";
+import { products as initialProducts } from "./mocks/products.json";
 import { useProducts } from "./hooks/useProducts";
 import { Spinner } from "./components/Spinner";
 
 function App() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(initialProducts);
   const { webProducts, getElements } = useProducts();
   const { filteredProducts } = useFilters();
+  const filterProducts = filteredProducts(initialProducts);
 
-  useEffect(() => {
+  /* useEffect(() => {
     console.log(webProducts);
     if (webProducts?.length === 0) {
       getElements();
@@ -25,16 +27,16 @@ function App() {
       const prods = { products: [webProducts] };
       setProducts(prods.products[0]);
     }
-  };
+  };*/
 
   return (
     <CartProvider>
       <Header />
       <Cart />
-      {webProducts?.length === 0 ? (
+      {products?.length === 0 ? (
         <Spinner />
       ) : (
-        <Products products={filteredProducts(products)} />
+        <Products products={filterProducts} />
       )}
     </CartProvider>
   );
