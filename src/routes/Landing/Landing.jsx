55cf9bbb321072cd7c3d48b74464c305389449bd
Federico Villace <federicovillace@gmail.com>
2023-04-01 13:06:27 -0300

    updated landing css

--- src/routes/Landing/Landing.jsx
+++ src/routes/Landing/Landing.jsx
@@ -7,19 +7,17 @@ export const Landing = () => {
       <ul className="ul">
         <li>
           <h1>Shopping Never been so easy!</h1>
-        </li>
-        <div>
           <h4>
             Find the product that best suits you and get it in your home with
             the fastest shipment.
           </h4>
-        </div>
-        <li>
-          <Link to={"/HomePage"}>
-            <button>Shop now!</button>
-          </Link>
         </li>
       </ul>
+      <div className="button-container">
+        <Link to={"/HomePage"}>
+          <button>Shop now!</button>
+        </Link>
+      </div>
     </div>
   );
 };
